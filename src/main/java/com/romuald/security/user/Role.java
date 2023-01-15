0a683ec1b117be02a5719fd5555c46f05bb68375
kaye romuald <kayeromy@gmail.com>
2023-01-15 15:52:31 +0100

    auth

---
+++ src/main/java/com/romuald/security/user/Role.java
@@ -0,0 +1,7 @@
+package com.romuald.security.user;
+
+public enum Role {
+
+    USER,
+    ADMIN
+}
---
+++ src/test/java/com/romuald/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romuald.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
